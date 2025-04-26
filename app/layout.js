4427a816a197f74566029cb78bc805a79829a0f9
Domintes <domintes@gmail.com>
2025-04-26 12:11:14 +0200

    fix

--- app/layout.js
+++ app/layout.js
@@ -3,13 +3,13 @@ import "./globals.scss";
 
 export const metadata = {
   title: 'Osuverse',
-  description: 'Osu! beatmaps collection manager',
+  description: 'Osu! beatmaps collection manager'
 };
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body>
+      <body className="app-body">
         <div style={{ display: 'flex', height: '100vh' }}>
           <Navigation />
           <main style={{ flex: 1, padding: '1rem' }}>
